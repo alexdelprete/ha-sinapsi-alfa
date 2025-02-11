3fc91b9680b67d108b13d60690c6942180fb68b7
Alessandro Del Prete <alessandro.delprete@gmail.com>
2025-02-11 03:05:28 +0100

    * Optimized unix_timestamp_to_iso8601_local_tz()

--- custom_components/sinapsi_alfa/helpers.py
+++ custom_components/sinapsi_alfa/helpers.py
@@ -5,8 +5,7 @@ https://github.com/alexdelprete/ha-sinapsi-alfa
 
 import ipaddress
 import re
-import time
-from datetime import datetime, timedelta, timezone
+from datetime import UTC, datetime
 
 
 def host_valid(host):
@@ -19,40 +18,23 @@ def host_valid(host):
         return all(x and not disallowed.search(x) for x in host.split("."))
 
 
-def get_local_timezone_offset() -> float:
-    """Get local timezone offset."""
-    # Get the current time in seconds since the epoch
-    current_time = time.time()
-
-    # Get the local timezone offset in seconds
-    local_timezone_offset_seconds = (
-        -time.timezone
-        if (time.localtime(current_time).tm_isdst == 0)
-        else -time.altzone
-    )
-
-    # Convert the offset to hours
-    local_timezone_offset_hours = local_timezone_offset_seconds / 3600
-
-    return local_timezone_offset_hours
-
-
 def unix_timestamp_to_iso8601_local_tz(unix_timestamp: int) -> str:
-    """Convert timestamp to ISO8601."""
+    """Convert unix timestamp (UTC) to datetime ISO8601 format with local timezone offset.
 
-    # Convert Unix timestamp to datetime object in UTC
-    dt_utc = datetime.utcfromtimestamp(unix_timestamp).replace(tzinfo=timezone.utc)  # noqa: UP017
+    Args:
+        unix_timestamp (int): Unix timestamp in seconds since the epoch.
 
-    # Get the local timezone offset
-    local_timezone_offset_hours = get_local_timezone_offset()
+    Returns:
+        str: ISO8601 formatted datetime string with local timezone offset.
 
-    # Create a timezone object with the local offset
-    local_timezone = timezone(timedelta(hours=local_timezone_offset_hours))
+    Example:
+        >>> unix_timestamp_to_iso8601_local_tz(1739238065) in CET timezone
+        '2025-02-11T02:41:05+01:00'
 
-    # Convert UTC datetime to local datetime
-    dt_local = dt_utc.astimezone(local_timezone)
+    """
 
-    # Format local datetime object as ISO8601 string
-    iso8601_format = dt_local.isoformat()
-
-    return iso8601_format
+    return (
+        datetime.fromtimestamp(timestamp=unix_timestamp, tz=UTC)
+        .astimezone()
+        .isoformat()
+    )
